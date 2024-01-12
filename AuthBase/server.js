f908fe51bcf65a41eb00e82274d4f37daa65ef5c
Adrian Manczak <manczakadrian@gmail.com>
2024-01-13 00:25:04 +0100

    update

--- AuthBase/server.js
+++ AuthBase/server.js
@@ -5,6 +5,35 @@ const hbs = require('express-handlebars');
 
 const app = express();
 
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const session = require('express-session');
+
+passport.use(new GoogleStrategy({
+  clientID: '9754491gfdgfdg5fdalmg9111fe3ahrbu.apps.googleusercontent.com'
+  clientSecret: 'OfHsdfsdfgMZTLtlIMoe0',
+  callbackURL: 'http://localhost:8000/auth/google/callback'
+}, (accessToken, refreshToken, profile, done) => {
+done(null, profile);
+}));
+
+app.use(session({ secret: 'anything' }));
+// serialize user when saving to session
+passport.serializeUser((user, serialize) => {
+  serialize(null, user);
+});
+// deserialize user when reading from session
+passport.deserializeUser((obj, deserialize) => {
+  deserialize(null, obj);
+});
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['email', 'profile'] }));
+
 app.engine('hbs', hbs({ extname: 'hbs', layoutsDir: './layouts', defaultLayout: 'main' }));
 app.set('view engine', '.hbs');
 
